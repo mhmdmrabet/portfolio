d1eb531f646010d45f0c673fa5e3638e717916a7
Mohamed <mhmdmrabet@yahoo.fr>
2021-12-16 18:59:06 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://avatars.githubusercontent.com/u/58339336?v=4"
+          className="rounded-full"
+          alt="avatar"
+        />
+        <p>Hi 👋, I'm Mohamed M'rabet</p>
+        <p>A passionate developer from Paris.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
