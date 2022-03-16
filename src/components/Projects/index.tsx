849869caa7d11e0a98288c0d9dfa87b21fbc73bc
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-16 10:35:32 +0100

    Add projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -5,19 +5,29 @@ export const Projects = () => {
       <ul className="my-10 space-y-10">
         <li className="bg-blue-700 opacity-60 rounded sm:h-64 flex flex-row">
           <div className="basis-2/6">
-            <img
-              src="https://i.ytimg.com/vi/SHTXpNfK2R8/maxresdefault.jpg"
-              alt=""
-              className="p-2 h-full"
-            />
+            <img src="./assets/weather-app.png" alt="weather app" className="p-2 h-full" />
           </div>
           <div className="basis-4/6">
-            <h3 className="text-black text-center my-6 font-bold text-xl">Wich Movie</h3>
+            <h3 className="text-black text-center my-6 font-bold text-xl">Weather App</h3>
             <div className="flex flex-col space-y-4">
-              <p className="text-justify p-2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat iure corporis
-                vitae.
-              </p>
+              <p className="text-justify p-2">App allowing to search weather infos.</p>
+              <a
+                href="https://github.com/mhmdmrabet/weather-app"
+                className="text-black text-right mr-4 underline"
+              >
+                Watch on Github
+              </a>
+            </div>
+          </div>
+        </li>
+        <li className="bg-blue-700 opacity-60 rounded sm:h-64 flex flex-row">
+          <div className="basis-2/6">
+            <img src="./assets/which-movie.png" alt="which movie app" className="p-2 h-full" />
+          </div>
+          <div className="basis-4/6">
+            <h3 className="text-black text-center my-6 font-bold text-xl">Which Movie</h3>
+            <div className="flex flex-col space-y-4">
+              <p className="text-justify p-2">App allowing to search the details of a film.</p>
               <a
                 href="https://github.com/mhmdmrabet/whichmovie"
                 className="text-black text-right mr-4 underline"
