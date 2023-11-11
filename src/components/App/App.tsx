8f460f03c96e78fe15995ebe4fcfd8b88854ed93
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2023-11-11 09:17:42 +0100

    feature(clean): Remove Nav & Keep only home page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import { About } from '../About';
 import { HomePage } from '../HomePage';
-import { Nav } from '../Nav';
-import { NotFound } from '../NotFound';
-import { Projects } from '../Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="bg-gray-800">
-      <Nav />
       <div className="h-screen text-white">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
